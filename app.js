1be364d03f5d772ce0212b626a527dffed5374c4
siyakhumalo-ms <107889389+siyakhumalo-ms@users.noreply.github.com>
2022-09-26 20:38:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure!!!');
+  res.send('Hello Siya from Azure ACR!!!');
 });
 
 app.listen(config.port, () => {
